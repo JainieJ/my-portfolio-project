04b744230a6c7cd901652fc0befeeb36b719b2d5
Janie <evgeniyaberseneva89@gmail.com>
2019-09-14 23:46:33 +0300

    Updates

---
+++ precache-manifest.fa906ef6e7e29eb129b06f2c3e8230fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df20bd3b213f259f292a652ae366a918",
+    "url": "/github.io/my-portfolio-project/index.html"
+  },
+  {
+    "revision": "93d3a064226fb12f2697",
+    "url": "/github.io/my-portfolio-project/static/css/main.b7a2b451.chunk.css"
+  },
+  {
+    "revision": "6981655decbb054aa348",
+    "url": "/github.io/my-portfolio-project/static/js/2.fd41426c.chunk.js"
+  },
+  {
+    "revision": "93d3a064226fb12f2697",
+    "url": "/github.io/my-portfolio-project/static/js/main.9628780e.chunk.js"
+  },
+  {
+    "revision": "2378e2a11848f6b701ec",
+    "url": "/github.io/my-portfolio-project/static/js/runtime~main.f676e1d5.js"
+  },
+  {
+    "revision": "4fc986929079179ec6035ddbf2a5694e",
+    "url": "/github.io/my-portfolio-project/static/media/coding-1200.4fc98692.jpg"
+  },
+  {
+    "revision": "8d9d8b27d5f6a5ac5290019eb4a0fd08",
+    "url": "/github.io/my-portfolio-project/static/media/coding.8d9d8b27.jpg"
+  },
+  {
+    "revision": "e91b6e7d651fbe75df4d608775d1f711",
+    "url": "/github.io/my-portfolio-project/static/media/russia.e91b6e7d.svg"
+  },
+  {
+    "revision": "386cc387066317cf209c9abf7e98afab",
+    "url": "/github.io/my-portfolio-project/static/media/united-kingdom.386cc387.svg"
+  }
+]);
